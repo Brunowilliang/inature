8987453c1e8ad8a9c875765457bdb9603e565bca
Bruno Garcia <brunowilliang@MacBook-Pro-de-Bruno-Garcia.local>
2023-10-09 03:20:07 +0100

    fix/third commit 2

--- src/components/useMediaQuery.ts
+++ src/components/useMediaQuery.ts
@@ -3,17 +3,21 @@
 import { useEffect, useState } from 'react';
 
 const useMediaQuery = (query: string): boolean => {
-  const [matches, setMatches] = useState(window.matchMedia(query).matches);
+  // Define um valor inicial para evitar a referência a `window` no lado do servidor
+  const [matches, setMatches] = useState(false);
 
   useEffect(() => {
-    const media = window.matchMedia(query);
-    if (media.matches !== matches) {
-      setMatches(media.matches);
+    // Só continua se window estiver definido (ou seja, estamos no lado do cliente)
+    if (typeof window !== 'undefined') {
+      const media = window.matchMedia(query);
+      setMatches(media.matches);  // Atualiza o estado inicial com o valor correto
+
+      const listener = () => setMatches(media.matches);
+      media.addEventListener('change', listener);
+
+      return () => media.removeEventListener('change', listener);
     }
-    const listener = () => setMatches(media.matches);
-    media.addEventListener('change', listener);
-    return () => media.removeEventListener('change', listener);
-  }, [matches, query]);
+  }, [query]);
 
   return matches;
 };
